fca9a7b9f6a4e14b5e6010e610102e176bbe0f6f
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-06-22 23:29:19 +0200

    Add files via upload

---
+++ StringEquals/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace StringEquals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string znak = "ż";
+            if(znak.Equals("z", StringComparison.InvariantCulture))
+            {
+                Console.WriteLine("Rowne");
+            }
+            else
+            {
+                Console.WriteLine("Nie rowne");
+            }
+
+            char[] array = new char[10];
+            Console.WriteLine(array[0]);
+        }
+    }
+}
